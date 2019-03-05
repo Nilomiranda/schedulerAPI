3b3cb59e6aa00243b0ead8c6e052228ff9a9e05e
Danilo Miranda <danilomiranda@Danilos-MacBook-Pro.local>
2019-03-05 17:49:23 -0300

    Adding feature to create a new event (migration, model, controller)

---
+++ app/Controllers/Http/EventController.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const Event = use('App/Models/Event')
+
+/**
+ * Resourceful controller for interacting with events
+ */
+class EventController {
+  /**
+   * Show a list of all events.
+   * GET events
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new event.
+   * POST events
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response, auth }) {
+    try {
+      const { title, location, date, time } = request.all() // info for the event
+      const userID = auth.user.id // retrieving user id current logged
+
+      const newEvent = await Event.create({ user_id: userID, title, location, date, time })
+
+      return newEvent
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ message: {
+          error: 'Something went wrong while creating new event'
+        } })
+    }
+  }
+
+  /**
+   * Update event details.
+   * PUT or PATCH events/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a event with id.
+   * DELETE events/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = EventController
--- start/routes.js
+++ start/routes.js
@@ -8,11 +8,14 @@ Route.get('users', 'SessionController.store') // user login
 // recover password route
 Route.post('users/forgotPassword', 'ForgotPasswordController.store')
 Route.put('users/forgotPassword/:token/:email', 'ForgotPasswordController.update')
+
 /**
  * routes only for authenticated users
  */
-
 Route.group(() => {
   // updating username and password
   Route.put('users/:id', 'UpdateUserInfoController.update')
+
+  // creating new Event
+  Route.post('/events/new', 'EventController.store')
 }).middleware(['auth'])
